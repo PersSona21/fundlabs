a2451a48b0a015decc4bb83c92629e8887a66fed
PersSona21 <perssona219@gmail.com>
2025-09-30 21:45:42 +0300

    edit 3

--- first_pack/3/main.c
+++ first_pack/3/main.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 #include "main.h"
 
 
@@ -17,8 +18,73 @@ bool validateFlag(const char *flag){
     return false;
 }
 
-void solveQE(double eps, double a, double b, double c){
-    return;
+return_code solveQE(double eps, double a, double b, double c){
+    printf("For eps = %f, a = %f, b = %f, c = %f\n", eps, a, b, c);
+    if (fabs(a) <= eps){
+        if (fabs(b) <= eps){
+            if (fabs(c) <= eps){
+                printf("Infinity of roots\n");
+            }
+            else{
+                printf("No roots\n");
+            }
+        }
+        else{
+            if (fabs(c) <= eps){
+                printf("x = 0\n");
+            }
+            else {
+                double x = -b / c;
+                printf("x = %f\n", x);
+            }
+        }
+    }
+    else{
+        double d = b * b - 4 * a * c;
+        if (d > eps){
+            double sd = sqrt(d);
+            double x1 = (-b + sd) / (2 * a);
+            double x2 = (-b - sd) / (2 * a);
+            printf("x1 = %f, x2 = %f\n", x1, x2);
+        }
+        else if (fabs(d) <= eps){
+            double x = -b / (2 * a);
+            printf("x = %f\n", x);
+        }
+        else{
+            printf("No real roots\n");
+        }
+    }
+    return OK;
+}
+
+return_code multiplicityCheck(int a, int b){
+    if (a == 0 || b == 0){
+        return NUMBER_ERROR;
+    }
+    if (a % b == 0){
+        printf("%d multiples of %d\n", a, b);
+    }
+    else{
+        printf("%d is not a multiples of %d\n", a, b);
+    }
+    return OK;
+}
+
+return_code isTriangle(double eps, double a, double b, double c){
+    if (fabs(a) <= eps || fabs(b) <= eps || fabs(c) <= eps){
+        printf("They can't be sides of a triangle\n");
+        return OK;
+    }
+    if (fabs(a * a + b * b - c * c) <= eps 
+    || fabs(a * a + c * c - b * b) <= eps 
+    || fabs(c * c + b * b - a * a) <= eps){
+        printf("They can be sides of a triangle\n");
+    }
+    else {
+        printf("They can't be sides of a triangle\n");
+    }
+    return OK;
 }
 
 int main(int argc, char* argv[]){
@@ -35,25 +101,39 @@ int main(int argc, char* argv[]){
     }
     switch (flag[1]) {
         case 'q':
-            if (argc < 6){
+            if (argc != 6){
                 printError("must be eps, a, b, c");
                 return INPUT_ERROR;
             }
-            // solveQE
+            double eps = atof(argv[2]);
+            double a = atof(argv[3]);
+            double b = atof(argv[4]);
+            double c = atof(argv[5]);
+            solveQE(eps, a, b, c);
             break;
         case 'm':
-            if (argc < 4){
+            if (argc != 4){
                 printError("must be a, b");
                 return INPUT_ERROR;
             }
-            // multiplicityCheck
+            int a1 = atoi(argv[2]);
+            int b1 = atoi(argv[3]);
+            if(multiplicityCheck(a1, b1) == NUMBER_ERROR){
+                printError("a or b can't equal 0");
+            }
             break;
         case 't':
-            if (argc < 6){
+            if (argc != 6){
                 printError("must be eps, a, b, c");
                 return INPUT_ERROR;
             }
-            // isTriangle
+            double eps2 = atof(argv[2]);
+            double a2 = atof(argv[3]);
+            double b2 = atof(argv[4]);
+            double c2 = atof(argv[5]);
+            if (isTriangle(eps2, a2, b2, c2)){
+                return FUNCTION_ERROR;
+            }
             break;
         default:
             printError("incorrect flag");
--- first_pack/3/main.h
+++ first_pack/3/main.h
@@ -5,13 +5,15 @@ typedef enum return_code{
     OK = 0,
     FLAG_ERROR = 1,
     NUMBER_ERROR = 2,
-    INPUT_ERROR = 3
+    INPUT_ERROR = 3,
+    DIVIDE_BY_ZERO = 4,
+    FUNCTION_ERROR = 5
 } return_code;
 
 void printError(char *str);
 bool validateFlag(const char *flag);
-void solveQE(double eps, double a, double b, double c);
-void multiplicityCheck(int a, int b);
-void isTriangle(double eps, double a, double b, double c);
+return_code solveQE(double eps, double a, double b, double c);
+return_code multiplicityCheck(int a, int b);
+return_code isTriangle(double eps, double a, double b, double c);
 
 #endif
